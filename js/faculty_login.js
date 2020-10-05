7c615a33a1cbd1c48aee8693c6a134409a39d30e
Shreya549 <shreya.chatterjee2018@vitstudent.ac.in>
2020-10-06 01:20:05 +0530

    Login API linked

---
+++ js/faculty_login.js
@@ -0,0 +1,30 @@
+function login(){
+    console.log('Hii')
+    // debugger;
+    var data = {
+        "empid" : document.getElementById('username').value,
+        "password" : document.getElementById('password').value
+    }
+
+    var xh = new XMLHttpRequest();
+    xh.open("POST", "https://achieve-vit.herokuapp.com/accounts/login/", true);
+    xh.setRequestHeader('Content-Type', 'application/json');
+    xh.send(JSON.stringify(data));
+    // xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
+    xh.onload = function () {
+        console.log("HIii")
+        console.log(this.responseText)
+        console.log(this.status)
+        // debugger;
+        if(this.status==200)
+        {
+            var data = JSON.parse(this.responseText)
+            localStorage.setItem("Token", "Token " + data.token)
+            window.location.replace('homepage_emp.html')
+        }
+        else{
+            alert('Invalid login credentials')
+            window.location.replace('login_employee.html')
+        }
+    }
+}
