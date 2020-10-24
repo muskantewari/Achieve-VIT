6e84917c3e1359cd03c1024779926ab649ad922c
Shreya Chatterjee <43725310+Shreya549@users.noreply.github.com>
2020-10-25 02:44:37 +0530

    Achievements linked

---
+++ js/my_profile.js
@@ -0,0 +1,31 @@
+function addAchievements()
+{
+    var data = {
+        "details" : document.getElementById("details").value
+    }
+    var jwt = localStorage.getItem('Token')
+
+    console.log(jwt)
+    var xh = new XMLHttpRequest();
+    xh.open("POST", "https://achieve-vit.herokuapp.com/portfolio/achievements/", true)
+    xh.setRequestHeader('Content-Type', 'application/json')
+    xh.setRequestHeader('Authorization', jwt);
+    xh.send(JSON.stringify(data));
+
+    xh.onload=function(){
+        console.log(this.responseText)
+        if(this.status==201)
+        {
+            var node = document.createElement('div')
+            node.setAttribute("class", "col-md-6 ml-5")
+            // node.innerHTML("new Achievement");
+            divReqd = document.getElementById("achievement").appendChild(node);
+        }
+        else if(this.status==401){
+            alert('Please authenticate user')
+        }
+        else{
+            alert("Could not save")
+        }
+}
+}
